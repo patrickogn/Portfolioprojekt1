a9e2a8f2330ce90716ecfebc53f5c3b690f799db
Patrick Nielsen <cph-pn157@cphbusiness.dk>
2023-04-04 00:29:15 +0200

    startkode done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Frontend Website</h2>
+
+        <p>very good frontend website for portfolio!</p>
+    
+      </div>
+     
+    
+    
+    </div>
+  )
+}
+
+export default App
